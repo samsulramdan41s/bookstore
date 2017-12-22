7900938f631eb73779b0767a1d58cade05df5306
samsulramdan41s <samsul@41studio.com>
2017-12-22 10:44:09 +0700

    Kaminari Pagination

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,6 +2,6 @@ class StoreController < ApplicationController
   include CurrentCart
   before_action :set_cart
   def index
-    @books = Book.order(:title)
+    @books = Book.order(:title).page(params[:page]).per(4)
   end
 end
